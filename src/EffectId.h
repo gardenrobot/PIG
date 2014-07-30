b78b0033863737d6e469e9a70d2be8976aedbe54
Sam Clotfelter <samclot@protonmail.com>
2014-07-30 11:49:31 -0400

    Changed EffectId from enumerations to macros.

--- src/EffectId.h
+++ src/EffectId.h
@@ -11,12 +11,13 @@
 #define EFFECT_ID_H_
 
 
-typedef enum
-{
-    NO_EFFECT = 0,
-    AFFLICT_BURN = 1,
-    INCREASE_SPEED_2 = 2,
-} EffectId;
+// macro for type
+#define EffectId int
+
+// macros for effect ids
+#define NO_EFFECT 0
+#define AFFLICT_BURN 1
+#define INCREASE_SPEED_2 2
 
 
 #endif // EFFECT_ID_H_
